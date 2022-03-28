7eb731f5fe59b9204c87d508c304099f7506ce0f
Icaro Davi <icarodaviduarte@gmail.com>
2022-03-28 07:56:10 -0300

    optmize secure in verify comment and arguments

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,20 +1,32 @@
+import { Message } from 'discord.js';
+import { BotUsage } from '../commands';
 import { DiscordBot } from '../config';
 import { splitCommandAndArgs } from '../utils/commands';
 import MD from '../utils/md';
 
+const itIsANormalMessage = (message: Message) => {
+    return (!message.content.startsWith(process.env.BOT_PREFIX || '!') || message.author.bot)
+}
+
+const searchBotCommand = (botCommand: string) => {
+    const commandFromAliases = DiscordBot.Commands.AliasesCollection.get(botCommand);
+    return DiscordBot.Commands.Collection.get(commandFromAliases || botCommand);
+}
+
+const anyArgumentIsRequired = (args: string[], usage: BotUsage) => {
+    const argsRequiredByIndex = usage.map(args => args.reduce((prev, current) => prev + Number(current.required), 0));
+    return !argsRequiredByIndex.every((requiredIndex, i) => requiredIndex ? i < args.length : true);
+}
+
 export default () =>
     DiscordBot.Client.get().on('messageCreate', (message) => {
-        if (!message.content.startsWith(process.env.BOT_PREFIX || '!') || message.author.bot) return;
+        if (itIsANormalMessage(message)) return;
         const command = splitCommandAndArgs(message.content);
-        const commandFromAliases = DiscordBot.Commands.AliasesCollection.get(command.name);
-        const botCommand = DiscordBot.Commands.Collection.get(commandFromAliases || command.name);
+        const botCommand = searchBotCommand(command.name);
         if (!botCommand) return;
         try {
-            const requiredCount = botCommand
-                .usage?.reduce((prev, current) => [...prev, ...current], [])
-                .reduce((prev, current) => current.required ? prev + 1 : prev, 0);
-            if (requiredCount && (requiredCount > command.args.length)) {
-                message.reply(`I do not understand what are you order to me, please use ${MD.codeBlock.line(process.env.BOT_PREFIX+'h '+command.name)} to learn about this command.`);
+            if (botCommand.usage && anyArgumentIsRequired(command.args, botCommand.usage)) {
+                message.reply(`I do not understand what are you order to me, please use ${MD.codeBlock.line(process.env.BOT_PREFIX + 'h ' + command.name)} to learn about this command.`);
                 return;
             }
             botCommand.exec(message, command.args);
