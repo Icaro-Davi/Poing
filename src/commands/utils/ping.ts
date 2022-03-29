defe963a3187ebf0d5359a8bf0abf1022f94fb64
Icaro Davi <icarodaviduarte@gmail.com>
2022-03-28 21:20:03 -0300

    ping user and discord api

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,19 +1,36 @@
-import { BotCommand, ExecuteCommand } from "..";
+import { MessageEmbed } from "discord.js";
+import { BotCommand } from "..";
+import MD from "../../utils/md";
 import { createGetHelp } from '../../utils/messageEmbed';
-
-const ping: ExecuteCommand = (message, args) => {
-    message.channel.send('...').then(resultMessage => (
-        message.reply(`...Pong, your latency ${resultMessage.createdTimestamp - message.createdTimestamp}ms`)
-    ));
-}
 
 const command: BotCommand = {
     name: 'ping',
     category: 'Utility',
     description: 'I will send you the time in milliseconds (ms) it takes me to reply to you.',
     aliases: ['p'],
+    usage: [
+        [{ required: false, arg: 'none', description: 'No need arguments', example: `${MD.codeBlock.line('{prefix}ping')} - return the time in milliseconds of my services.` }]
+    ],
     getHelp: (customPrefix) => createGetHelp(command, customPrefix),
-    exec: ping
+    exec: async (message, args) => {
+        const resultMessage = await message.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setColor(`#${process.env.BOT_MESSAGE_EMBED_HEX_COLOR}`)
+                    .setTitle(':small_orange_diamond: Ping')
+            ]
+        });
+        let clientPing = `Your ping: ${resultMessage.createdTimestamp - message.createdTimestamp}ms`;
+        let serverPing = `@${process.env.BOT_NAME} to server: ${message.client.ws.ping}ms`;
+        resultMessage.edit({
+            embeds: [
+                new MessageEmbed()
+                    .setColor(`#${process.env.BOT_MESSAGE_EMBED_HEX_COLOR}`)
+                    .setTitle(':small_blue_diamond: Pong')
+                    .setDescription(`${clientPing}\n${serverPing}`)
+            ]
+        });
+    }
 }
 
 export default command;
