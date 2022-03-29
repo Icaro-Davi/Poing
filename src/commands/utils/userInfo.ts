af68fed77c3aafa29d42f382d50fb65be6ec6899
Icaro Davi <icarodaviduarte@gmail.com>
2022-03-28 21:23:32 -0300

    added get user by memberId

--- src/commands/utils/userInfo.ts
+++ src/commands/utils/userInfo.ts
@@ -5,7 +5,7 @@ import { BotCommand } from '..';
 import MD from '../../utils/md';
 import { createGetHelp } from '../../utils/messageEmbed';
 
-const memberMessageEmbed = async (member: GuildMember) => {    
+const memberMessageEmbed = async (member: GuildMember) => {
     return new MessageEmbed()
         .setColor(`#${process.env.BOT_MESSAGE_EMBED_HEX_COLOR}`)
         .setTitle(`Tag ${member.user.username}#${member.user.discriminator}`)
@@ -17,30 +17,47 @@ const memberMessageEmbed = async (member: GuildMember) => {
         .setThumbnail(member.user.avatarURL() || '')
 }
 
-const handleMember = async (message: Message, args: string[]): Promise<GuildMember | undefined> => {
-    if (args.length) {
-        return message.mentions.members?.first() ||
-            (await message.guild?.members.search({ query: args.join(' '), limit: 1 }))?.first();
+const searchMember = async (message: Message, args: string[]): Promise<GuildMember | undefined> => {
+    try {
+        if (args.length) {
+            if (!Number.isNaN(Number(args.join(' ')))){
+                const member = (await message.guild?.members.fetch({ user: args.join(' '), limit: 1 }));                
+                if(member?.user) return member;
+            }
+            return message.mentions.members?.first() ||
+                (await message.guild?.members.search({ query: args.join(' '), limit: 1 }))?.first();
+        }
+        return (await searchMember(message, [message.author.username]));
+    } catch (error) {
+        console.log(error)
+        message.channel.send('I could not complete this adventure to search your member friend, something wrong.')
     }
-    return (await handleMember(message, [message.author.username]));
 }
 
 const command: BotCommand = {
     name: 'userinfo',
     category: 'Utility',
     aliases: ['whois', 'who', 'user'],
-    description: 'Get from your server a member info',
+    description: 'Look up information about a specific member on the server.',
     usage: [
-        [{
-            required: false, arg: 'user',
-            description: `The reference of any member from the server like ${MD.codeBlock.line('[mention | username | nickname | memberId]')}.`
-        }],
+        [
+            {
+                required: false, arg: 'user',
+                description: `The reference of any server member, can be ${MD.codeBlock.line('[mention | username | nickname | memberId]')}.`,
+                example: `${MD.codeBlock.line(`{prefix}userinfo @${process.env.BOT_NAME}`)} - Will return information about @${process.env.BOT_NAME}.`
+            },
+            {
+                required: false, arg: 'none',
+                description: `No need arguments, will return info about you.`,
+                example: `${MD.codeBlock.line('{prefix}userinfo')} - Will return your information.`
+            }
+        ],
     ],
     getHelp: (customPrefix) => createGetHelp(command, customPrefix || process.env.BOT_PREFIX),
     exec: async (message, args) => {
-        const member = await handleMember(message, args);
+        const member = await searchMember(message, args);        
         if (member) return message.reply({ embeds: [await memberMessageEmbed(member)] });
-        return message.reply('Sorry, i do not found any member');
+        return message.reply('Sorry, I could not find any members');
     }
 }
 
