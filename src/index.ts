31765cefa616dd82e0850b5a351984e98877461c
Icaro Davi <icarodaviduarte@gmail.com>
2022-03-30 21:36:41 -0300

    added intents

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,9 @@ DiscordBot.Client.set(new DiscordJS.Client({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_PRESENCES,        
-    ]
+        Intents.FLAGS.GUILD_PRESENCES,      
+        Intents.FLAGS.GUILD_BANS,             
+    ],    
 }));
 
 const { aliasesCommandsKey, clientCommands } = getAllBotCommands();
