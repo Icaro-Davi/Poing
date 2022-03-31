a09fe2f76af7f2f6e685ba191abf44c3b53f0e60
Icaro Davi <icarodaviduarte@gmail.com>
2022-03-30 21:40:57 -0300

    added command kick member

---
+++ src/commands/moderation/kick.ts
@@ -0,0 +1,43 @@
+import { BotCommand } from "..";
+import { Member } from "../../application";
+import MD from "../../utils/md";
+import { createGetHelp } from "../../utils/messageEmbed";
+
+const command: BotCommand = {
+    name: 'kick',
+    category: 'Moderation',
+    description: 'Kick a member from the server.',
+    allowedPermissions: ['ADMINISTRATOR', 'KICK_MEMBERS'],
+    usage: [
+        [{
+            required: true, arg: 'user',
+            description: `You can use ${MD.codeBlock.line('[mention | username | nickname | memberId]')}`,
+            example: `${MD.codeBlock.line('{prefix}kick @Poing')} - User @Poing will be kicked from the server.`
+        }],
+        [{
+            required: false, arg: 'reason=',
+            description: `Reason for kick the member from the server, ${MD.codeBlock.line('[reason= | --r=]')}.`,
+            example: `${MD.codeBlock.line('{prefix}kick @Poing reason=Poing is jumping through the server.')} - It will kick @Poing with a reason.`
+        }]
+    ],
+    getHelp: (customPrefix) => createGetHelp(command, customPrefix),
+    exec: async (message, args) => {
+        let argReasonIndex = args.join(' ').search((/(reason=|--r=)/gi));
+        let user = undefined;
+        let reason = undefined;
+        if (argReasonIndex > 0) {
+            user = args.join(' ').slice(0, argReasonIndex).trim();
+            reason = args.join(' ').slice(argReasonIndex).replace(/(reason=|--r=)/gi, '').trim();
+        }
+        const member = await Member.search(message, user ? [user] : args);
+        if (member) {
+            if (member.kickable) member.kick(reason).then((_member) => {
+                return message.channel.send(`${_member.user.username} was kicked!`);
+            });
+            else return message.channel.send('I cannot kick this member.');
+        }
+        message.channel.send('User not found');
+    }
+};
+
+export default command;
