495e7bb1a351d12e3637a882c781bf3c6ebad1ab
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-01 17:21:08 -0300

    add itents

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ DiscordBot.Client.set(new DiscordJS.Client({
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_PRESENCES,      
-        Intents.FLAGS.GUILD_BANS,             
+        Intents.FLAGS.GUILD_BANS,   
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS          
     ],    
 }));
 
