8efb924e15f305589b338838db28e168a80a2f67
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-01 17:23:57 -0300

    search using memberId ou mention

--- src/application/member.ts
+++ src/application/member.ts
@@ -1,16 +1,13 @@
 import { GuildMember, Message } from "discord.js";
 
 class Member {
-    static async search(message: Message, args: string[]): Promise<GuildMember | undefined> {
-        if (args.length) {
-            if (!Number.isNaN(Number(args[0]))) {
-                const member = await message.guild?.members.fetch({ user: args.join(' '), limit: 1 });
-                if (member?.user) return member;
-            }
-            return message.mentions.members?.first() ||
-                (await message.guild?.members.search({ query: args.join(' '), limit: 1 }))?.first();
+    static async search(message: Message, member: string): Promise<GuildMember | undefined> {
+        if (!Number.isNaN(Number(member))) {
+            const _member = await message.guild?.members.fetch({ user: member, limit: 1 });
+            if (_member?.user) return _member;
         }
-        return undefined;
+        return message.mentions.members?.first() ||
+            (await message.guild?.members.search({ query: member, limit: 1 }))?.first();
     }
 }
 
