6a4ac7f2d2343c44ca7e60571205c0ac249273ff
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-03 13:15:37 -0300

    adde event listener

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,9 @@
+import { DiscordBot } from "../config/";
+
+export default () =>
+    DiscordBot.Client.get().on('interactionCreate', async (interaction) => {
+        // if(interaction.isButton()){
+        //     if(interaction.customId === 'yes' || interaction.customId === 'no')
+        //         await interaction.reply('Done!');
+        // }
+    });
