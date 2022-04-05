77ec9a43cb8fdd9d1e39b26ee137708ef84f9205
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-05 16:41:07 -0300

    send options to exec command

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,8 +1,10 @@
 import { Message, PermissionResolvable } from 'discord.js';
+
 import { BotUsage } from '../commands';
 import { DiscordBot } from '../config';
 import { splitCommandAndArgs } from '../utils/commands';
 import MD from '../utils/md';
+import translateCommandToLocale from '../locale';
 
 const itIsANormalMessage = (message: Message) => {
     return (!message.content.startsWith(process.env.BOT_PREFIX || '!') || message.author.bot)
@@ -28,6 +30,7 @@ export default () =>
         const command = splitCommandAndArgs(message.content);
         const botCommand = searchBotCommand(command.name);
         if (!botCommand) return;
+        const locale = await translateCommandToLocale({ ...botCommand }, 'pt-BR');
         try {
             if (botCommand.usage && anyArgumentIsRequired(command.args, botCommand.usage)) {
                 message.reply(`I do not understand what are you order to me, please use ${MD.codeBlock.line(process.env.BOT_PREFIX + 'help ' + command.name)} to learn about this command.`);
@@ -37,9 +40,16 @@ export default () =>
                 message.reply('You does not have permission to execute this command.');
                 return;
             }
-            await botCommand.exec(message, command.args);
+            await locale.botCommand.exec(message, command.args, {
+                locale: locale.get,
+                bot: {
+                    name: DiscordBot.Bot.nickname,
+                    prefix: DiscordBot.Bot.defaultPrefix,
+                    hexColor: DiscordBot.Bot.defaultBotHexColor,
+                }
+            });
         } catch (error) {
             console.error(error);
-            message.reply(`Sorry, i think i have a bug in that command, i will try to kill it.`);
+            await message.reply(`Sorry, i think i have a bug in that command, i will try to kill it.`);
         }
     });
