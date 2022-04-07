88082d9066f34ce25809cc17b7338a6209a8eea6
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-07 14:00:39 -0300

    change import ref

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -3,8 +3,7 @@ import { Member } from "../../application";
 import getValuesFromStringFlag from "../../utils/regex/getValuesFromStringFlag";
 import { replaceVarsInString } from "../../locale";
 import { confirmButtons } from "../../components/messageActionRow";
-import { confirmBanishMember } from "../../components/messageEmbed";
-import messageToBanishedMember from "../../components/messageEmbed/messageToBanishedMember";
+import { confirm, PM } from "../../components/messageEmbed";
 import handleError from "../../utils/handleError";
 
 const command: BotCommand = {
@@ -30,6 +29,10 @@ const command: BotCommand = {
         }]
     ],
     exec: async (message, args, options) => {
+        const member = await Member.search(message, args[0]);
+        if (!member) return await message.channel.send(options.locale.interaction.memberNotFound);
+        if (!member.bannable) return await message.channel.send(options.locale.interaction.memberIsNotBannable);
+        
         const days = getValuesFromStringFlag(args, ['-days', '--d']);
         const reason = getValuesFromStringFlag(args, ['-reason', '--r']);
 
@@ -38,14 +41,10 @@ const command: BotCommand = {
             else if (Number(days) > 7 || Number(days) < 0) return await message.channel.send(replaceVarsInString(`[-days | --d] - ${options.locale.interaction.numberMustBeBetweenTwoValues}`, { number1: 0, number2: 7 }));
         }
 
-        const member = await Member.search(message, args[0]);
-        if (!member) return await message.channel.send(options.locale.interaction.memberNotFound);
-        if (!member.bannable) return await message.channel.send(options.locale.interaction.memberIsNotBannable);
-
         const component = confirmButtons({ locale: options.locale });
 
         const interactionMessage = await message.channel.send({
-            embeds: [confirmBanishMember({
+            embeds: [confirm.banMember({
                 locale: options.locale, botColor: options.bot.hexColor,
                 authorTag: message.author.tag, authorUrl: message.author.avatarURL() || '',
                 days: Number(days), reason, member
@@ -72,13 +71,20 @@ const command: BotCommand = {
                     promises.push(buttonInteraction.first()?.reply(options.locale.interaction.memberBanishedFromServer));
                     await Promise.all(promises);
                     member.send({
-                        embeds: [messageToBanishedMember({
+                        embeds: [PM.toBanishedMember({
                             botColor: options.bot.hexColor, locale: options.locale,
                             guildName: message.guild?.name || '', iconUrl: message.guild?.iconURL() || '',
                             reason
                         })],
-                    }).catch(() => { });
-                    await interactionMessage.edit({ content: '🔨', embeds: [], components: [] });
+                    }).catch((error) => {
+                        handleError(error, {
+                            errorLocale: '/command/moderation/ban',
+                            locale: options.locale,
+                            message: message,
+                            customMessage: error.code === '50007' ? options.locale.command.ban.error[50007] : '' 
+                        });
+                    });
+                    await interactionMessage.edit({ content: '🔨', embeds: [] });
                 }
                 if (buttonId === component.button.noId) {
                     await interactionMessage.edit({ content: '🎈', embeds: [], components: [] });
@@ -93,7 +99,6 @@ const command: BotCommand = {
                 });
             }
         });
-
     }
 }
 
