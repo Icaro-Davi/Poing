bf92e3893801771faf6f5c951fa18b0e983aa32f
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-07 17:25:20 -0300

    adjust kick member to confirm before kick

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -1,34 +1,81 @@
 import { BotCommand } from "..";
 import { Member } from "../../application";
-import MD from "../../utils/md";
+import { confirmButtons } from "../../components/messageActionRow";
+import { confirm, PM } from "../../components/messageEmbed";
+import { onlyMessageAuthorCanUse } from "../../utils/collectorFlters";
+import handleError from "../../utils/handleError";
 
 const command: BotCommand = {
     name: 'kick',
-    category: 'Moderation',
-    description: 'Kick a member from the server.',
+    category: '{category.moderation}',
+    description: '{command.kick.description}',
     allowedPermissions: ['KICK_MEMBERS'],
     usage: [
         [{
-            required: true, arg: 'member',
-            description: `You can use ${MD.codeBlock.line('[mention | memberId]')}`,
-            example: `${MD.codeBlock.line('{prefix}kick @Poing')} - User @Poing will be kicked from the server (Please does not kick me ;w;).`
+            required: true, arg: '{usage.member.arg}',
+            description: '{usage.member.description}',
+            example: '{command.kick.usage.memberExample}'
         }],
         [{
-            required: false, arg: 'reason',
-            description: `Reason for kick the member from the server'.`,
-            example: `${MD.codeBlock.line('{prefix}kick @Poing Poing is jumping through the server.')} - It will kick @Poing with a reason.(Do not do that ;w;)`
+            required: false, arg: '{usage.reason.arg}',
+            description: '{usage.reason.description}',
+            example: '{command.kick.usage.reasonExample}'
         }]
     ],
-    exec: async (message, args) => {
+    exec: async (message, args, options) => {
         const member = await Member.search(message, args[0]);
-        if (member) {
-            if (member.kickable) {
-                const kikedMember = await member.kick(args.slice(1).join(' ') || '');
-                return kikedMember ? await message.channel.send(`Bye ${kikedMember.user.username}`) : false;
+        if (!member) return await message.channel.send(options.locale.interaction.member.notFound);
+        if (!member.kickable) return await message.channel.send(options.locale.interaction.member.isNotKickable);
+
+        const reason = args.slice(1).join(' ');
+        const component = confirmButtons({ locale: options.locale });
+
+        const interactionMessage = await message.channel.send({
+            embeds: [confirm.kickMember({
+                botColor: options.bot.hexColor,
+                locale: options.locale, authorTag: message.author.tag, authorUrl: message.author.avatarURL() || '',
+                member, reason
+            })],
+            components: [component.row]
+        });
+
+        const collector = onlyMessageAuthorCanUse(message, options.locale);
+
+        collector.on('end', async memberInteraction => {
+            try {
+                const componentId = memberInteraction.first()?.customId;
+                if (componentId === component.button.yesId) {
+                    await member.kick(reason || '');
+                    await memberInteraction.first()?.reply(options.locale.interaction.member.kickFromServer);
+                    member.send({
+                        embeds: [PM.toKickedMember({
+                            botColor: options.bot.hexColor, guildName: message.guild?.name || '',
+                            iconUrl: message.guild?.iconURL() || '',
+                            locale: options.locale,
+                            reason
+                        })]
+                    }).catch(error => handleError(error, {
+                        errorLocale: '/commands/moderation/kick',
+                        locale: options.locale,
+                        message,
+                        customMessage: error.code == '50007' ? options.locale.command.kick.error[50007] : ''
+                    }));
+                    await interactionMessage.edit({ content: '🦶💢', components: [] });
+                    return;
+                }
+                if (componentId === component.button.noId) {
+                    await interactionMessage.edit({ content: '🎈', embeds: [], components: [] });
+                    await message.channel.send(options.locale.interaction.member.kickCanceled);
+                    return;
+                }
+            } catch (error) {
+                handleError(error, {
+                    locale: options.locale,
+                    errorLocale: '/commands/moderation/kick',
+                    message,
+                });
             }
-            return await message.channel.send('I cannot kick this member.');
-        }
-        return await message.channel.send('User not found');
+        });
     }
 };
 
