f5ad69139a97503203ce5ed9d98a657724bb4ad2
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-07 01:58:08 -0300

    added new message embed

--- src/components/messageEmbed/index.ts
+++ src/components/messageEmbed/index.ts
@@ -1,8 +1,10 @@
+import confirmBanishMember from "./confirmBanishMember";
 import createGetHelp from "./createGetHelp";
 import listCommandsByCategory from "./listCommandsByCategory";
 import privateMessage from "./privateMessage";
 
 export {
+    confirmBanishMember,
     createGetHelp,
     listCommandsByCategory,
     privateMessage
