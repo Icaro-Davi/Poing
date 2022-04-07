8c515da2837e6796086138252299f58a332d4d6f
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-07 19:33:01 -0300

    implement locale

--- src/commands/moderation/removeMessages.ts
+++ src/commands/moderation/removeMessages.ts
@@ -1,21 +1,24 @@
 import { BotCommand } from "..";
-import MD from "../../utils/md";
 
 const command: BotCommand = {
     name: 'remove-messages',
-    category: 'Moderation',
-    description: 'Remove messages from any channel, discord limit bots to remove messages until 2 weeks ago.',
+    category: '{category.moderation}',
+    description: '{command.removeMessages.description}',
     aliases: ['rm'],
     usage: [
-        [{ required: true, arg: 'quantity', description: 'Quantity of messages to remove', example: `${MD.codeBlock.line('{prefix}rm 5')} - Will remove 5 last messages.` }]
+        [{
+            required: true, arg: '{usage.quantity.arg}',
+            description: '{usage.quantity.description}',
+            example: '{command.removeMessages.usage.quantityExample}'
+        }]
     ],
     allowedPermissions: ['MANAGE_MESSAGES'],
-    exec: async (message, args) => {
+    exec: async (message, args, options) => {        
         if (message.channel.type == 'DM') return false;
-        if (!Number.isNaN(Number(args[0]))) {
-            const deletedMessages = await message.channel.bulkDelete(Number(args[0]), true);
-            return message.channel.send(`Deleted ${deletedMessages.size} messages`);
-        }
+        if (Number.isNaN(Number(args[0]))) return await message.channel.send(options.locale.interaction.mustBeNumber);
+
+        const deletedMessages = await message.channel.bulkDelete(Number(args[0]), true);
+        return await message.channel.send(options.locale.command.removeMessages.interaction.replace('{deletedMessageSize}', deletedMessages.size.toString()));
     }
 }
 
