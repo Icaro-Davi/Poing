8e212803eefd7e40035d2e86d9483830d0dde1d2
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-09 23:29:17 -0300

    implements translate

--- src/commands/utils/getMembersStatus.ts
+++ src/commands/utils/getMembersStatus.ts
@@ -1,26 +1,28 @@
 import { Message, MessageEmbed } from 'discord.js';
-import { BotCommand } from '..';
+import { BotCommand, ExecuteCommandOptions } from '..';
 import MD from '../../utils/md';
 
-const getAllMembers = async (message: Message) => {
-    const membersStatusCount = await message.guild?.members.cache
+const getAllMembers = async (message: Message, options: ExecuteCommandOptions) => {
+    const membersStatusCount = (await message.guild?.members.fetch())?.toJSON()
         .reduce<{ [key: string]: number }>((prev, member) => {
             if (member.presence?.status) {
-                if (prev[member.presence.status]) prev[member.presence.status] += 1;
-                else prev[member.presence.status] = 1;
+                prev[member.presence.status] ? prev[member.presence.status] += 1 : prev[member.presence.status] = 1;
+            } else {
+                prev['offline'] ? prev['offline'] += 1 : prev['offline'] = 1;
             }
             return prev;
         }, {});
-    let onlineMemberText = `${MD.bold.b(':green_circle: Online:')} ${membersStatusCount?.online || 0}`;
-    let idleMemberText = `${MD.bold.b(':yellow_circle: Idle:')} ${membersStatusCount?.idle || 0}`;
-    let dndMemberText = `${MD.bold.b(':red_circle: Do not disturb:')} ${membersStatusCount?.dnd || 0}`;
-    let offlineMemberText = `${MD.bold.b(':white_circle: Offline:')} ${membersStatusCount?.offline || 0}`;
-    let totalMemberText = `${MD.bold.b(':blue_circle: Total:')} ${message.guild?.memberCount || 0}`;
+    console.log(membersStatusCount)
+    let onlineMemberText = `${MD.bold.b(`:green_circle: ${options.locale.status.online}:`)} ${membersStatusCount?.online || 0}`;
+    let idleMemberText = `${MD.bold.b(`:yellow_circle: ${options.locale.status.idle}:`)} ${membersStatusCount?.idle || 0}`;
+    let dndMemberText = `${MD.bold.b(`:red_circle: ${options.locale.status.dnd}:`)} ${membersStatusCount?.dnd || 0}`;
+    let offlineMemberText = `${MD.bold.b(`:white_circle: ${options.locale.status.offline}:`)} ${membersStatusCount?.offline || 0}`;
+    let totalMemberText = `${MD.bold.b(`:blue_circle: ${options.locale.interaction.member.total}:`)} ${message.guild?.memberCount || 0}`;
     message.reply({
         embeds: [
             new MessageEmbed()
-                .setColor(`#${process.env.BOT_MESSAGE_EMBED_HEX_COLOR}`)
-                .setTitle('Member Status')
+                .setColor(options.bot.hexColor)
+                .setTitle(options.locale.messageEmbed.memberStatus.title)
                 .setDescription(`${onlineMemberText}\n${idleMemberText}\n${dndMemberText}\n${offlineMemberText}\n${totalMemberText}`)
         ]
     });
@@ -28,11 +30,11 @@ const getAllMembers = async (message: Message) => {
 
 const command: BotCommand = {
     name: 'get-members-status',
-    category: 'Utility',
-    description: 'I will return the amount of online members in this server.',
+    category: '{category.utility}',
+    description: '{command.getMembersStatus.description}',
     aliases: ['gms'],
-    exec: (message, args) => {
-        getAllMembers(message);
+    exec: (message, args, options) => {
+        getAllMembers(message, options);
     }
 }
 
