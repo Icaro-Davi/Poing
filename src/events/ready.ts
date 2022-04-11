c56a5c1c9a8af38587d1c6a1619f96bf8b7162ad
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-11 14:41:09 -0300

    added color to project start logs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -9,10 +9,12 @@ export default () =>
             name: 'Poing Poing Poing',
             url: 'https://github.com/icaro-davi'
         });
-        console.log('\n\n', fs.readFileSync('./draw').toString())
+        let commands = searchCommandsFiles('./src/commands');
+        console.log( '\x1b[95m','\n\n', fs.readFileSync('./draw').toString(),'\x1b[0m')
         console.log(`\n\n\n-- I'm listening all :3`);
         console.log('--- Prefix:', process.env.BOT_PREFIX);
         console.log('--- Bot Name:', process.env.BOT_NAME);
-        console.log('--- Loaded Commands:', searchCommandsFiles('./src/commands'));
+        console.log('--- Quantity of Commands:', commands.length);
+        console.log('--- Loaded Commands:', '\x1b[34m', commands.map(path => `[${path.split(/(?:\\|\/)/g).pop()}]`).join(' '), '\x1b[0m');
         console.log(`-- My creator is https://github.com/icaro-davi`);
     });
