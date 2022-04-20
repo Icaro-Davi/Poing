b857d48f49937433355035c21c8b9a5e11e623a8
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-20 09:38:52 -0300

    imports

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,15 @@
 import Bot from './Bot';
 import Client from './client';
 import Commands from './commands';
+import GuildMemory from './guildMemory';
+import ScheduleEvent from './schedule';
 
 const DiscordBot = {
     Bot,
     Client,
-    Commands
+    Commands,
+    GuildMemory,
+    ScheduleEvent
 };
 
 export { DiscordBot };
