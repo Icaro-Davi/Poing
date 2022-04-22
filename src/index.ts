db92728ed57caaff71407f1ea1472bdd9850ee04
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-22 19:37:56 -0300

    change intents

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,11 @@ DiscordBot.Client.set(new DiscordJS.Client({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_PRESENCES,      
-        Intents.FLAGS.GUILD_BANS,   
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_BANS,
         Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS          
-    ],    
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ],
 }));
 
 const { aliasesCommandsKey, clientCommands } = getAllBotCommands();
