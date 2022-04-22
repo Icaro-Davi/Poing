325295586bafa0338910109a190454e83d9a533b
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-22 19:42:58 -0300

    connect database

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,11 +1,13 @@
 import Discord from 'discord.js';
+import { DiscordBot } from '.';
 import startListeningEvents from '../events';
 
 class Client {
     private static client: Discord.Client;
-    static start = () => {
+    static async start() {        
         startListeningEvents();
-        this.client.login(process.env.BOT_TOKEN);
+        await DiscordBot.Database.start();
+        await this.client.login(process.env.BOT_TOKEN);
     }
     static get = () => this.client;
     static set = (client: Discord.Client) => { this.client = client; }
---
+++ src/config/database.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+class Database {
+    private static mongoDBOnline = false;
+
+    static async start() {
+        await this.connectMongoDB();
+    }
+
+    private static async connectMongoDB() {
+        try {
+            mongoose.connection.on('connected', () => {
+                this.mongoDBOnline = true;
+            });
+            mongoose.connection.on('error', err => {
+                console.error('[database connectMongoDB error] Could not connect to mongoDB, exiting process...', err);
+                process.exit(1);
+            });
+            await mongoose.connect(process.env.DATABASE_MONGODB!);
+        } catch (error) {
+            console.error('[database connectMongoDB error]', error);
+        }
+    }
+
+    static getMongoDBStatus() {
+        return this.mongoDBOnline ? 'Online' : 'Offline';
+    }
+}
+
+export default Database;
