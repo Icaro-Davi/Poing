a1294318a9bcc5111bbc6baf7494fe0873ce58d1
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-25 01:29:43 -0300

    test search command

--- __tests__/events/createMessage.spec.ts
+++ __tests__/events/createMessage.spec.ts
@@ -1,9 +1,8 @@
-import { config } from 'dotenv';
 import { Message } from "discord.js";
-import { itIsANormalMessage } from "../../src/events/messageCreate";
-import { splitCommandAndArgs } from "../../src/utils/commands";
+import { DiscordBot } from "../../src/config";
+import { itIsANormalMessage, searchBotCommand } from "../../src/events/messageCreate";
+import { getAllBotCommands, splitCommandAndArgs } from "../../src/utils/commands";
 
-config();
 
 describe('Event Create Message', () => {
     const message = ({
@@ -11,11 +10,10 @@ describe('Event Create Message', () => {
             bot: false,
         },
         content: '',
-
     } as unknown) as Message;
     it('It should be a command', () => {
         message.content = '!userinfo';
-        expect(itIsANormalMessage(message, '!')).toBe(false);
+        expect(itIsANormalMessage(message, '!')).toBeFalsy();
     });
     it('Should be a normal message or ignore bots message', () => {
         message.content = 'This is a normal message from any user';
@@ -24,10 +22,26 @@ describe('Event Create Message', () => {
         expect(itIsANormalMessage(message, '!')).toBeTruthy();
     });
     it('Should remove prefix, cut message content and get command and list of arguments', () => {
-        message.content = `${process.env.BOT_PREFIX}command arg1 arg2`;
-        const botCommand = splitCommandAndArgs(message.content);
+        message.content = `!command arg1 arg2`;
+        const botCommand = splitCommandAndArgs(message.content, '!');
         expect(botCommand.name).toBe('command');
         expect(botCommand.args).toBeInstanceOf(Array);
         expect(botCommand.args.length).toBe(2);
+    });
+    it('Should search a command', () => {
+        DiscordBot.Commands.start();
+        const commands = getAllBotCommands();
+
+        const randomCommandNumber = Math.round(Math.random() * (1 - commands.clientCommands.size) + 1);
+        const randomAliasesNumber = Math.round(Math.random() * (1 - commands.aliasesCommandsKey.size) + 1);
+
+        const randomCommandKey = commands.clientCommands.keyAt(randomCommandNumber) as string;
+        const randomAliasesKey = commands.clientCommands.keyAt(randomAliasesNumber) as string;
+
+        const botCommand = searchBotCommand(randomCommandKey);
+        expect(botCommand?.name).toBe(randomCommandKey);
+
+        const aliasesCommand = searchBotCommand(randomAliasesKey);
+        expect(aliasesCommand?.name).toBe(randomAliasesKey);
     });
 }); 
