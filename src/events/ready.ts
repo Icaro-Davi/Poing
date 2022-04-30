7edbe89a1d019855c9ccd238cb7ddcb744f70c64
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-30 19:54:39 -0300

    log size of guild connecteds

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -15,8 +15,9 @@ export default () =>
         console.log('--- Default Prefix:', process.env.BOT_PREFIX);
         console.log('--- Bot Name:', process.env.BOT_NAME);
         console.log('--- Quantity of Commands:', commands.length);
-        console.log('--- Loaded Commands:', '\x1b[34m', commands.map(path => `[${path.split(/(?:\\|\/)/g).pop()?.replace(/(?:\.ts|\.js)/, '')}]`).join(' '), '\x1b[0m');
-        console.log('--- Schedule Events started', DiscordBot.ScheduleEvent.start());
+        console.log('--- Guilds:', DiscordBot.Client.get().guilds.cache.size);
+        // console.log('--- Loaded Commands:', '\x1b[34m', commands.map(path => `[${path.split(/(?:\\|\/)/g).pop()?.replace(/(?:\.ts|\.js)/, '')}]`).join(' '), '\x1b[0m');
+        console.log('--- Schedule Events', DiscordBot.ScheduleEvent.start() ? 'Online' : 'Offline');
         console.log('--- MongoDB status', DiscordBot.Database.getMongoDBStatus());
         console.log(`-- My creator is https://github.com/icaro-davi`);
     });
