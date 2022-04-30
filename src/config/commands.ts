0bc5c4c10044381bad44876b49177bc3989c0677
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-30 19:55:33 -0300

    load commands on class Commands

--- src/config/commands.ts
+++ src/config/commands.ts
@@ -1,11 +1,21 @@
 import Discord from 'discord.js';
 import { BotCommand } from '../commands';
+import { getAllBotCommands } from '../utils/commands';
 
 class Commands {
     public static Collection: Discord.Collection<string, BotCommand>;
     public static AliasesCollection: Discord.Collection<string, string>;
-    static setCollection = (commands: Discord.Collection<string, BotCommand>) => { this.Collection = commands; }
-    static setAliases = (aliases: Discord.Collection<string, string>) => { this.AliasesCollection = aliases; }
+
+    static start = () => {
+        this.startPrefixCommands();
+    }
+
+    private static startPrefixCommands(){
+        const { aliasesCommandsKey, clientCommands } = getAllBotCommands();
+        this.Collection = clientCommands;
+        this.AliasesCollection = aliasesCommandsKey;
+    }
+
 }
 
 export default Commands;
