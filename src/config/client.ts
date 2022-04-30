0500e43183e343cdb35c145fc20f0035f2b5b99d
Icaro Davi <icarodaviduarte@gmail.com>
2022-04-30 19:52:59 -0300

    move logic to Client config

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,16 +1,29 @@
-import Discord from 'discord.js';
+import Discord, { Intents } from 'discord.js';
 import { DiscordBot } from '.';
 import startListeningEvents from '../events';
 
 class Client {
-    private static client: Discord.Client;
-    static async start() {        
+
+    private static client: Discord.Client = new Discord.Client({
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_PRESENCES,
+            Intents.FLAGS.GUILD_BANS,
+            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        ],
+    });
+
+    static async start() {
         startListeningEvents();
         await DiscordBot.Database.start();
         await this.client.login(process.env.BOT_TOKEN);
+        DiscordBot.Commands.start();
     }
+
     static get = () => this.client;
-    static set = (client: Discord.Client) => { this.client = client; }
 }
 
 export default Client;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
-import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 import { DiscordBot } from './config';
-import { getAllBotCommands } from './utils/commands';
-
-DiscordBot.Client.set(new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-    ],
-}));
-
-const { aliasesCommandsKey, clientCommands } = getAllBotCommands();
-DiscordBot.Commands.setCollection(clientCommands);
-DiscordBot.Commands.setAliases(aliasesCommandsKey);
 
 DiscordBot.Client.start();
