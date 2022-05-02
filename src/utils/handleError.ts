f259a336717d591edd9b583176ad48dedf9dea9e
Icaro Davi <icarodaviduarte@gmail.com>
2022-05-02 15:56:19 -0300

    change to reply

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -9,8 +9,8 @@ type HandleErrorOptions = {
 }
 
 const handleError = (error: any, options: HandleErrorOptions) => {
-    console.error(`[error on ${options.errorLocale}] - code(${error.code || 'no code'}) message(${error.message || 'no message'}) userInput(${options.message.content})`);    
-    options.message.channel.send(options.customMessage || options.locale.error[error.code as LocaleErroTypes || 'unknown']).catch(reason => console.log('[error on handleError] - Error on send message', reason));
+    console.error(`[error on ${options.errorLocale}] - code(${error.code || 'no code'}) message(${error.message || 'no message'}) userInput(${options.message.content})`);
+    options.message.reply(options.customMessage || options.locale.error[error.code as LocaleErroTypes || 'unknown']).catch(reason => console.log('[error on handleError] - Error on send message', reason));
 }
 
 export default handleError;
