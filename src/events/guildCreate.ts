ef4911129d2e2ce93f0dbd3c42078b4110a2ffcc
Icaro Davi <icarodaviduarte@gmail.com>
2022-05-05 18:34:40 -0300

    added welcome guild message

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,13 +1,25 @@
 
-import { Guild, TextChannel } from "discord.js";
+import { Guild, MessageEmbed, TextChannel } from "discord.js";
 import { GuildApplication } from "../application";
 import { DiscordBot } from "../config";
+import { getAvailableLocales, getInitialLocaleVars, LocaleLabel, replaceVarsInString } from "../locale";
+
+export const welcomeGuild = async (guild: Guild) => {
+    const channel = guild.channels.cache.find(channel => channel.isText()) as TextChannel;
+    const localeLang = (getAvailableLocales().some(locale => guild.preferredLocale === locale) ? guild.preferredLocale : 'en-US') as LocaleLabel;
+    await GuildApplication.create(guild.id, localeLang);
+    const defaultLocaleVars = await getInitialLocaleVars({ locale: localeLang });
+    await channel.send({
+        embeds: [new MessageEmbed({
+            color: DiscordBot.Bot.defaultBotHexColor,
+            description: replaceVarsInString(defaultLocaleVars.locale?.interaction.welcomeGuild!, { bot: defaultLocaleVars.bot })
+        })]
+    });
+}
 
 const onBotJoinGuild = async (guild: Guild) => {
     try {
-        await GuildApplication.create(guild.id);
-        const channel = guild.channels.cache.find(channel => channel.isText()) as TextChannel;
-        await channel.send(`Hello you invite me to join in your server, i hope to help you :3\nIf you wanna see my commands just use <@${guild.me?.id}> help, or !help`);
+        await welcomeGuild(guild);
     } catch (error) {
         console.error(error);
     }
