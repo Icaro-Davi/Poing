7a04ba1181280902225354f3547c50a77d21482c
Icaro Davi <icarodaviduarte@gmail.com>
2022-05-16 19:45:03 -0300

    remove search

--- src/application/member.ts
+++ src/application/member.ts
@@ -1,15 +1,6 @@
-import { Guild, GuildMember, Message } from "discord.js";
+import { Guild } from "discord.js";
 
 class Member {
-    static async search(message: Message, member: string): Promise<GuildMember | undefined> {
-        if (!Number.isNaN(Number(member))) {
-            const _member = await message.guild?.members.fetch({ user: member, limit: 1 });
-            if (_member?.user) return _member;
-        }
-        return message.mentions.members?.first() ||
-            (await message.guild?.members.search({ query: member, limit: 1 }))?.first();
-    }
-
     static async find({ guild, member }: { member: string, guild: Guild }) {
         if (!Number.isNaN(Number(member))) {
             const _member = await guild?.members.fetch({ user: member, limit: 1 });
