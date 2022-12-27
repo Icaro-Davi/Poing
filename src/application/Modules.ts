b4d1e5cabc73be76f2e2e0704821054e43efc838
Icaro Davi <icarodaviduarte@gmail.com>
2022-12-26 23:34:06 -0300

    fix: welcome module not sending correct message

--- src/application/Modules.ts
+++ src/application/Modules.ts
@@ -1,22 +1,9 @@
-import type { IWelcomeMemberModuleSettings } from "../domain/modules/welcomeModule/WelcomeModule.schema";
+import WelcomeModuleRepository from "../domain/modules/welcomeModule/WelcomeModuleRepository.mongo";
 
 class Modules {
 
-    static async getWelcomeConfig(guildId: string) {
-        return new Promise<IWelcomeMemberModuleSettings>((resolve, reject) => {
-            setInterval(() => resolve({
-                isMessageText: false,
-                channelId: '1024774617073733743',
-                messageText: '👋 **Olá {member.mention}** \nBem vindo ao servidor. \n\nDivirta-se!!',
-                messageEmbed: {
-                    author: { name: `{member.username}`, picture: '{member.picture}' },
-                    title: `👋 Olá {member.username}`,
-                    description: `Bem vindo ao servidor.`,
-                    thumbnail: `{member.picture}`,
-                    footer: 'Divirta-se!!',
-                }
-            }), 100);
-        })
+    static async getWelcomeSettingsById(moduleId: string) {
+        return await WelcomeModuleRepository.findById(moduleId);
     }
 
 }
