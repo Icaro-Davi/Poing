1201f71f4d8baea7de9e2648c61afb5ca2ed4f31
Icaro Davi <icarodaviduarte@gmail.com>
2023-02-06 23:51:36 -0300

    change: handle events

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,11 +1,16 @@
 import { createNewEvent } from ".";
 import execSlashCommand from "../commands/command.slash";
+import RoleByInteraction from "../modules/roleByInteraction.module";
 
 export default createNewEvent('interactionCreate', async (event, interaction) => {
     try {
         if (interaction.user.bot) return;
         if (interaction.isCommand()) {
             await execSlashCommand(interaction);
+        } else if (interaction.isButton()) {
+            if (await RoleByInteraction.validateEvent(event, true).exec(interaction)) return;
+        } else if (interaction.isSelectMenu()){
+            if (await RoleByInteraction.validateEvent(event, true).exec(interaction)) return;
         }
     } catch (error) {
         console.error('[EVENT_INTERACTION_CREATE] Error on src.events.interactionCreate \n', error);
