aa30f82839773197955ab9417881fffc741ad8ad
Icaro Davi <icarodaviduarte@gmail.com>
2023-03-11 03:02:53 -0300

    feat: handle pipeline

--- src/modules/textCommand.module.ts
+++ src/modules/textCommand.module.ts
@@ -1,26 +1,30 @@
 import { DiscordBot } from '../config';
 import handleError from '../utils/handleError';
 import getCommand from '../commands/command.default';
+import createPipeline from '../commands/command.middleware';
 
 import type { Message } from 'discord.js';
 import { createNewModule } from '.';
 
 const textCommands = async (message: Message) => {
     const Command = await getCommand(message);
-
     try {
         if (!Command) return;
         const { args, command, options } = Command;
-
-        const returnMessageOptions = await command?.execDefault(message, args, options);
-
-        await DiscordBot.Command.handleMessage({
-            ...returnMessageOptions, message,
-            vars: {
-                ...returnMessageOptions?.vars ? returnMessageOptions.vars : {},
-                ...options,
-            }
-        });
+        if (command.commandPipeline) {
+            const runPipeline = await createPipeline('COMMAND', command.commandPipeline);
+            await runPipeline.call(command, message, args, options);
+        } else {
+            const returnMessageOptions = await command?.execDefault?.(message, args, options);
+            if (returnMessageOptions)
+                await DiscordBot.Command.handleMessage({
+                    ...returnMessageOptions, message,
+                    vars: {
+                        ...returnMessageOptions?.vars ? returnMessageOptions.vars : {},
+                        ...options,
+                    }
+                });
+        }
     } catch (error) {
         Command?.options.locale ? handleError(error, {
             errorLocale: 'event/messageCreate',
