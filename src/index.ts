232d238ca35588f2650529913351532f468dd2c9
Icaro Davi <icarodaviduarte@gmail.com>
2023-03-16 00:36:02 -0300

    fix: bug on start project in production

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
+import pkg from '../package.json';
 import path from 'path';
 import fs from 'fs';
 
-const prodPath = path.resolve(__dirname, '../', '.env');
+const getRoot = (_path: string): string => {
+    const splitPath = path.resolve(_path).split(/\\|\//);
+    if (splitPath[splitPath.length - 1].toLocaleLowerCase() === pkg.name) return _path;
+    else {
+        splitPath.pop();
+        return getRoot(splitPath.join('/'));
+    }
+}
+const prodPath = path.resolve(getRoot(__dirname), '.env');
 const envFilePath = (fs.existsSync(prodPath)) ? prodPath : path.resolve(__dirname, '../', '.env.development.local');
 
 dotenv.config({ path: envFilePath, debug: true });
